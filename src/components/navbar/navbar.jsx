fff9405dbf4c88818cc04b5d2f9083d595e8e99d
bhardwajsahab101-boop <bhardwajsahab101@gmail.com>
2026-03-23 11:46:02 +0530

    this is my updated code of my profile app

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -12,11 +12,13 @@ function Navbar() {
   return (
     <div className="navbar_container">
       <div className="navbar_logo_container">
+        <Link to="/" className="navbar_logo_link">
+
         <img
           className="navbar_logo"
           src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgZzSOT8itwg4W5AroY2Oz58h_cPhPlFbklOCG-61a2bX5pyyzoLSNN7v7CB4sUF2MHgPVU4rqj9tmdR4kYV2237tacwXOCpjIlyB27dr3J1cbsfMTwCqR4asHTm-Ck9KKMsm3932ibi4uvuO3yM6txIBTBCnkRIPM_BD71pUMAyL2exNB2fEOJBigkpRE/s320/Monogram%20Logo%20with%20'AS'%20in%20Glassmorphism%20Style.png"
           alt="Logo"
-        />
+        /></Link>
       </div> 
       <div className={`nav-content ${isMenuOpen ? 'active' : ''}`}>
         <ul className="navbar_links_ul">
